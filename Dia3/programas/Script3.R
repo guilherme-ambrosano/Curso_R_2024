3dbc753db3843ff4edf17af49a824e7330b4bd7c
Gui Bovi <guibovi2@gmail.com>
2024-08-10 11:40:17 -0300

    Atualizando os scripts do dia 3

--- Dia3/programas/Script3.R
+++ Dia3/programas/Script3.R
@@ -9,10 +9,35 @@ modelo.aov <- aov(Sepal.Length ~ Species, data=iris)
 modelo.lm  <- lm(Sepal.Width ~ Petal.Width, data=iris)
 modelo.wt  <- wilcox.test(iris$Petal.Length, g=iris$Species)
 
+tidy(modelo.aov)
+glance(modelo.aov)
+
 # purrr
+
+# Função split e group_split - transformam tibble em list
+caribbean.maize %>%
+  split(.$isle)
+
+caribbean.maize %>%
+  group_split(isle) %>%
+  map(function(x) x %>%
+        pivot_wider(id_cols=c(block, plot),
+                    names_from=site,
+                    values_from=yield))
+
+# Exemplo das bolsas
 
 excel_sheets("dados/Relação nominal dos alunos - Bolsa Extensão - 2023.xlsx")
 arquivo_xlsx <- "dados/Relação nominal dos alunos - Bolsa Extensão - 2023.xlsx"
+
+bolsas <- map_dfr(excel_sheets(arquivo_xlsx),
+    function(x) read_xlsx(arquivo_xlsx, sheet=x) %>%
+      mutate(`VALOR DA BOLSA` = as.character(`VALOR DA BOLSA`),
+             Mes = x)) %>%
+  mutate(`VALOR DA BOLSA` = str_replace(`VALOR DA BOLSA`, ",", ".")) %>% 
+  mutate(`VALOR DA BOLSA` = parse_number(`VALOR DA BOLSA`)) %>%
+  mutate(Mes = str_replace(Mes, "APR", "ABR")) %>% 
+  mutate(Mes = parse_date_time2(Mes, orders = "%B-%Y"))
 
 map_dfr(excel_sheets(arquivo_xlsx),
         \(planilha) {
@@ -24,7 +49,49 @@ map_dfr(excel_sheets(arquivo_xlsx),
   mutate_at(vars(Mes), ~ifelse(.=="ABR-2023", "APR-2023", .)) %>%
   mutate_at(vars(Mes), parse_date_time2, "%B-%Y")
 
+# Fazendo a mesma operação em várias variáveis de uma vez
+iris %>%
+  select(-Species) %>%
+  as.list() %>% # as.list() transforma data.frame em list
+  map_dfr(. %>%
+            enframe() %>% 
+            mutate(Species = iris$Species) %>%
+            aov(value ~ Species, data=.) %>%
+            residuals() %>%
+            shapiro.test() %>% 
+            tidy(), .id = "Variável") %>%
+  select(Variável, p.value) %>%
+  mutate(signif = case_when(p.value<.001 ~ "***",
+                            p.value<.01 ~ "**",
+                            p.value<.05 ~ "*",
+                            T ~ "ns"))
+
+iris %>%
+  select(-Species) %>%
+  cor()
+
+cor.test(iris$Sepal.Length, iris$Sepal.Width)
+
+expand_grid(Var1=names(iris[-5]),
+            Var2=names(iris[-5])) %>%
+  pmap_dfr(function(Var1, Var2) {
+    tibble(Var1=Var1,
+           Var2=Var2,
+           p.value=cor.test(iris[,Var1],
+                            iris[,Var2])$p.value)
+  }) %>%
+  pivot_wider(id_cols=Var1, names_from=Var2,
+              values_from=p.value) %>%
+  column_to_rownames("Var1") %>%
+  as.matrix()
+
 # ggplot2
+
+iris %>%
+  ggplot(aes(col=Species,x=Sepal.Width, y=Sepal.Length)) +
+  geom_point() +
+  guides(color = guide_legend(direction = "vertical")) +
+  theme(legend.position = "bottom")
 
 dados_meteorologicos <- read_lines(
   "http://www.leb.esalq.usp.br/leb/exceldados/DCE2023.TXT") %>%
@@ -44,6 +111,58 @@ dados_meteorologicos <- read_lines(
   mutate_at(vars(-Data), parse_number) %>%
   filter(!is.na(Data))
 
+dados_meteorologicos %>%
+  ggplot(aes(x=Data, y=`TEMPER MEDIA`)) +
+  geom_line() +
+  scale_x_date(date_breaks="6 months",
+               date_labels = "%m-%Y")
+
+
+dados_meteorologicos %>%
+  select(Data, PRECIPITACAO, starts_with("TEMPER")) %>%
+  pivot_longer(starts_with("TEMPER")) %>%
+  ggplot(aes(x=Data, y=PRECIPITACAO)) +
+  geom_bar(stat="unique") +
+  geom_line(aes(col=name, y=value))
+
+
+iris %>%
+  pivot_longer(-Species) %>%
+  ggplot(aes(x=Species, y=value)) +
+  geom_boxplot() +
+  facet_wrap(~name)
+
+NBA.game %>%
+  group_by(TEAM) %>%
+  summarise_at(vars(W), list(média=mean,
+                             erro=\(x)sd(x/sqrt(length(x))))) %>%
+  mutate(TEAM = fct_reorder(TEAM, desc(média))) %>% 
+  #arrange(média) %>%
+  #slice(1:5) %>% 
+  ggplot(aes(x=TEAM, y=média)) +
+  geom_col() +
+  geom_errorbar(aes(ymin=média-erro, ymax=média+erro))
+
+# Exercício 1
+
+attitude %>% 
+  cor() %>%
+  as.data.frame() %>%
+  rownames_to_column() %>%
+  pivot_longer(-rowname) %>% 
+  mutate_at(vars(rowname, name), str_to_title) %>%
+  mutate(rowname.num = as.numeric(factor(rowname)), 
+         name.num = as.numeric(factor(name))) %>%
+  filter(rowname.num > name.num) %>%
+  mutate_at(vars(value), round, 4) %>% 
+  mutate(cor = ifelse(abs(value)<.45, "white", "black")) %>% 
+  ggplot(aes(x=rowname, y=name, fill=value)) +
+  geom_tile(height=.9, width=.9) +
+  geom_text(aes(label=value, color=cor), show.legend = F) +
+  scale_color_manual(values=c("grey10", "grey90")) +
+  theme_test() +
+  theme(axis.text.x = element_text(angle=90, hjust=1, vjust=.5))
+
 # Exemplos ----
 
 invasoras <- read_xlsx("dados/listagem-de-plantas-alternativas-as-plantas-exoticas-invasoras-listadas-para-o-estado-do-rio-de.xlsx",
@@ -55,12 +174,33 @@ invasoras <- read_xlsx("dados/listagem-de-plantas-alternativas-as-plantas-exotic
                                    "Ecossist_EEI", "Categoria"),
                        range="A8:L268")
 
+
+invasoras %>%
+  pivot_longer(c(ends_with("Ombro"),
+                 ends_with("Semidec"),
+                 ends_with("InfMar"))) %>% 
+  separate(name, c("Variável", "Ecossistema")) %>% 
+  pivot_wider(names_from="Variável",
+              id_cols=c(Codigo, ends_with("EEI"), 
+                        Categoria, Ecossistema)) %>%
+  # Values from `value` are not uniquely identified; 
+  #output will contain list-cols.
+  unnest_longer(c(NomeCient, NouE)) %>% # o `unnest` desfaz isso
+  filter(!is.na(NomeCient), !is.na(NouE))
+
+# Algumas plantas da mesma espécie aparecem com ecossistemas diferentes
+invasoras %>%
+  group_by(NomeCient_EEI, Ecossist_EEI) %>%
+  count() %>%
+  group_by(NomeCient_EEI) %>%
+  count()
+
 ecossist <- invasoras %>%
   mutate_at(vars(Ecossist_EEI), replace_na, "?") %>% 
   group_by(NomeCient_EEI, Ecossist_EEI) %>%
   count() %>%
   group_by(NomeCient_EEI) %>%
-  arrange(Ecossist_EEI) %>% 
+  arrange(Ecossist_EEI) %>% # arrumando os ecossistemas
   summarise(Ecossist_EEI = (function(x) {
     if(length(x)==1) return(x)
     else {
@@ -85,34 +225,7 @@ fomentoRural <- read_csv(
          "rows=1000000&",
          "wt=csv"))
 
-fr <- fomentoRural %>% 
-  select_if(~mean(is.na(.)|.==0)<.5) %>%
-  select(mes_ano, matches("qtd\\_pes\\_(cad\\_nao\\_)?pbf\\_idade\\_.+\\_sexo.+\\_i")) %>%
-  group_by(mes_ano) %>%
-  summarise_all(sum, na.rm=T) %>% 
-  pivot_longer(-mes_ano) %>%
-  mutate(pbf = str_extract(name, "(nao\\_)?pbf"),
-         idade=str_extract(name, "idade\\_(maior\\_que\\_)?\\d+(\\_[ea]\\_\\d+)?"),
-         sexo =str_extract(name, "sexo\\_[^\\_]+ino")) %>%
-  mutate(pbf = factor(pbf, levels=c("pbf", "nao_pbf"), labels=c("PBF", "Não-PBF")),
-         idade=str_replace_all(str_remove(idade, "idade\\_"), "\\_", " "),
-         sexo =str_to_title(str_remove(sexo, "sexo\\_"))) %>%
-  mutate(idade1 = parse_number(str_extract(idade, "\\d+"))) %>% 
-  mutate_at(vars(idade, sexo), factor) %>% 
-  mutate(idade = fct_reorder(idade, idade1)) %>% 
-  select(mes_ano, pbf, idade, sexo, name, value) %>%
-  arrange(mes_ano, pbf, idade, sexo) %>%
-  pivot_wider(id_cols=c(mes_ano, idade, sexo), names_from=pbf)
-
-fr %>%
-  ggplot(aes(x=mes_ano, col=idade, linetype=sexo, y=PBF)) +
-  geom_line()
-
-fr %>%
-  ggplot(aes(x=mes_ano, col=idade, linetype=sexo, y=`Não-PBF`)) +
-  geom_line()
-
-indicadores <- read_xlsx("../dados/ranking_2015_2020.xlsx")
+indicadores <- read_xlsx("dados/ranking_2015_2020.xlsx")
 estados <- read_state(
   year = 2020, 
   showProgress = FALSE)
@@ -125,12 +238,63 @@ indicadores %>%
   summarise(soma = sum(`Nota Normalizada Indicador`)) %>%
   mutate(`Ano Publicação` = factor(`Ano Publicação`)) %>% 
   mutate(Pilar = fct_reorder(Pilar, -soma)) %>% 
-  mutate(Pilar = fct_relabel(Pilar, str_wrap, width=15)) %>% 
+  mutate(Pilar = fct_relabel(Pilar, str_wrap, width=10)) %>% 
   ggplot(aes(x=Pilar, y=soma, color=`Ano Publicação`)) +
   geom_line(aes(group=`Ano Publicação`)) +
-  coord_radial(expand=F, r_axis_inside = T) +
+  coord_radial(expand=F, r_axis_inside = T) + # transforma as coordenadas
   theme_minimal() +
   theme(axis.title = element_blank())
+
+# Dois jeitos de mudar os nomes na legenda:
+dados_meteorologicos %>%
+  select(Data, PRECIPITACAO, starts_with("TEMPER")) %>% 
+  pivot_longer(c(-Data, -PRECIPITACAO)) %>% 
+  #mutate(name = factor(name, levels=c("TEMPER MINIMA",
+  #                                    "TEMPER MEDIA",
+  #                                    "TEMPER MAXIMA"),
+  #                     labels=c("Mínima", "Média", "Máxima"))) %>%
+  #mutate(name = fct_relabel(name, \(x) paste("Temperatura", x))) %>% 
+  ggplot(aes(x=Data, y=PRECIPITACAO)) +
+  geom_col() +
+  geom_line(aes(y=value, col=name)) +
+  #scale_color_manual(values=c("blue", "black", "red"),
+  #                   breaks=c("TEMPER MINIMA",
+  #                            "TEMPER MEDIA",
+  #                            "TEMPER MAXIMA"),
+  #                   labels=c("Temperatura mínima",
+  #                            "Média", "Max"),
+  #                   name="") +
+  theme(legend.position = "bottom")
+
+# Mudando os nomes antes do ggplot:
+dados_meteorologicos %>%
+  select(Data, PRECIPITACAO, starts_with("TEMPER")) %>% 
+  pivot_longer(c(-Data, -PRECIPITACAO)) %>% 
+  mutate(name = factor(name, levels=c("TEMPER MINIMA",
+                                      "TEMPER MEDIA",
+                                      "TEMPER MAXIMA"),
+                       labels=c("Mínima", "Média", "Máxima"))) %>%
+  mutate(name = fct_relabel(name, \(x) paste("Temperatura", x))) %>% 
+  ggplot(aes(x=Data, y=PRECIPITACAO)) +
+  geom_col() +
+  geom_line(aes(y=value, col=name)) +
+  theme(legend.position = "bottom")
+
+# Mudando os nomes pelo ggplot:
+dados_meteorologicos %>%
+  select(Data, PRECIPITACAO, starts_with("TEMPER")) %>% 
+  pivot_longer(c(-Data, -PRECIPITACAO)) %>% 
+  ggplot(aes(x=Data, y=PRECIPITACAO)) +
+  geom_col() +
+  geom_line(aes(y=value, col=name)) +
+  scale_color_manual(values=c("blue", "black", "red"),
+                     breaks=c("TEMPER MINIMA",
+                              "TEMPER MEDIA",
+                              "TEMPER MAXIMA"),
+                     labels=c("Temperatura mínima",
+                              "Média", "Max"),
+                     name="") +
+  theme(legend.position = "bottom")
 
 # Mais conjuntos de dados -----
 
